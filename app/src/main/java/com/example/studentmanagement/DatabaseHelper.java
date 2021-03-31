a99bb41c5f1fca2b2e69ff3e6b6d813c3366866a
hkawinzi <happinesskawinzi@gmail.com>
2021-03-31 09:37:22 +0300

    creating colums in the table

--- app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
+++ app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
@@ -9,6 +9,10 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "student.db";
     public static final String TABLE_NAME = "student table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
     public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
