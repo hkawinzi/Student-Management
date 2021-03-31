e836922d405e83e836455c5a11cc47bedccedf66
hkawinzi <happinesskawinzi@gmail.com>
2021-03-31 10:00:34 +0300

    command that will check if the database can be created or dropped

--- app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
+++ app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "student.db";
-    public static final String TABLE_NAME = "student table";
+    public static final String TABLE_NAME = "student_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
     public static final String COL_3 = "SURNAME";
@@ -16,15 +16,20 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db =this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " +TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
+
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
+        onCreate(db);
 
     }
 }
