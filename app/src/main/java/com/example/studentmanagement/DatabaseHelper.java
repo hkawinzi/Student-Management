a819c903d6c0b2d36d6f9b01955c66406f057461
hkawinzi <happinesskawinzi@gmail.com>
2021-03-31 09:22:29 +0300

    importing classes and adding a method that will enable us to use the main class for handling databases

--- app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
+++ app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
@@ -1,4 +1,23 @@
 package com.example.studentmanagement;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
