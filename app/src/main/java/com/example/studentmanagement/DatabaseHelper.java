2a85e3ef1c24c88dfe57e73e7abc0edfdac754ce
hkawinzi <happinesskawinzi@gmail.com>
2021-03-31 09:34:09 +0300

    creating a table name

--- app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
+++ app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "student table";
     public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
