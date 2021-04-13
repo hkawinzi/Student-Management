5430e7d3b6c76985bed2087827c8e2da8cfc4d34
Nzyoka <rose.nzyoka@thepalladiumgroup.com>
2021-04-13 14:04:07 +0300

    creating a method that will enable values to be insterted to the DB

--- app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
+++ app/src/main/java/com/example/studentmanagement/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.studentmanagement;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -16,7 +17,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db =this.getWritableDatabase();
+
     }
 
     @Override
@@ -30,6 +31,14 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
         onCreate(db);
+    }
 
+    public boolean insertData(String name,String surname,String marks){
+        SQLiteDatabase db =this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+        db.insert(TABLE_NAME,null, contentValues);
     }
 }
