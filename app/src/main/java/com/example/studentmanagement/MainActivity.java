cb73ca84daf40734f0c16d9282a7c97a7314fcfe
hkawinzi <happinesskawinzi@gmail.com>
2021-04-13 14:39:03 +0300

    casting the textview and button variables in the oncreate method

--- app/src/main/java/com/example/studentmanagement/MainActivity.java
+++ app/src/main/java/com/example/studentmanagement/MainActivity.java
@@ -8,13 +8,19 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
-    EditText editName, editSurname,editmarks;
+    EditText editName,editSurname,editMarks;
     Button btnAddData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new DatabaseHelper(this);
+
+        editName = (EditText)findViewById(R.id.editText_name);
+        editSurname = (EditText)findViewById(R.id.editText_surname);
+        editMarks = (EditText)findViewById(R.id.editText_Marks);
+        btnAddData = (Button) findViewById(R.id.button_add);
+
     }
 
 }
